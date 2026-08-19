5ebce7256741887ad2d207282524173682d331d3
Dai-yangjie <e1481826@u.nus.edu>
2026-08-19 23:04:51 +0800

    Rename main class to EV, add greeting and exit

---
+++ src/main/java/EV.java
@@ -0,0 +1,19 @@
+public class EV {
+    public static void main(String[] args) {
+        String banner = " _______     __\n"
+                + "|   ____|   /  \\\n"
+                + "|  |__     |    |\n"
+                + "|   __|    |    |\n"
+                + "|  |____    \\  /\n"
+                + "|_______|    \\/\n";
+        System.out.println(banner);
+
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm EV.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
